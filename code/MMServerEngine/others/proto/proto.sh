8e7123d0f0192ec22bc70dbaaaf8f7b10cd7b768
zhengyuzhen <>
2017-09-11 23:41:33 +0800

    proto生成

--- code/MMServerEngine/others/proto/proto.sh
+++ code/MMServerEngine/others/proto/proto.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 #cd `pwd`;
 echo "start proto...";
-protoc --java_out=./ ./protos/MiGongPB.proto;
-python idcreator.py ./protos/MiGongPB.proto MiGongOpcode.java 12001
+#csharp_out
+#protoc --csharp_out=./ ./protos/MiGongPB.proto;
+#protoc --java_out=./ ./protos/MiGongPB.proto;
+#python idcreator.py ./protos/MiGongPB.proto MiGongOpcode.java 12001
+python ./csharp/idcreator.py ./protos/MiGongPB.proto MiGongOpcode.cs 12001 MiGongBuild.cs
 echo "end proto...";
 read -n1 -p "Press any key to continue...";
