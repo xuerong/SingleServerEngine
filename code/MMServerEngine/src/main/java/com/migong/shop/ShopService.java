ee2023b328c2f57cad544ef5fefa52e02103f853
zhengyuzhen <zhengyuzhen@elex-tech.com>
2017-12-13 11:15:48 +0800

    migong

--- code/MMServerEngine/src/main/java/com/migong/shop/ShopService.java
+++ code/MMServerEngine/src/main/java/com/migong/shop/ShopService.java
@@ -14,15 +14,18 @@ import com.mm.engine.framework.net.code.RetPacketImpl;
 import com.mm.engine.framework.security.LocalizationMessage;
 import com.mm.engine.framework.security.exception.MMException;
 import com.mm.engine.framework.security.exception.ToClientException;
+import com.mm.engine.framework.server.IdService;
 import com.mm.engine.framework.tool.util.Util;
 import com.protocol.MiGongOpcode;
 import com.protocol.MiGongPB;
 import com.table.ItemTable;
 import com.table.PeckTable;
 import com.table.UnitTable;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,6 +35,7 @@ public class ShopService {
     private MiGongService miGongService;
     private DataService dataService;
     private ItemService itemService;
+    private IdService idService;
 
     private Map<Integer,ItemTable> itemTableMap = new HashMap<>();
     private Map<Integer,UnitTable> unitTableMap = new HashMap<>();
@@ -47,7 +51,7 @@ public class ShopService {
             peckTableMap.put(peckTable.getId(),peckTable);
         }
     }
-
+    @Tx
     @Request(opcode = MiGongOpcode.CSGoldBuy)
     public RetPacket goldBuy(Object clientData, Session session) throws Throwable{
         MiGongPB.CSGoldBuy goldBuy = MiGongPB.CSGoldBuy.parseFrom((byte[])clientData);
@@ -88,13 +92,88 @@ public class ShopService {
 
     @Request(opcode = MiGongOpcode.CSMoneyBuyBefore)
     public RetPacket moneyBuyBefore(Object clientData, Session session) throws Throwable{
-        MiGongPB.CSMoneyBuyBefore goldBuy = MiGongPB.CSMoneyBuyBefore.parseFrom((byte[])clientData);
-        log.info("");
+        MiGongPB.CSMoneyBuyBefore moneyBuyBefore = MiGongPB.CSMoneyBuyBefore.parseFrom((byte[])clientData);
+        PeckTable peckTable = peckTableMap.get(moneyBuyBefore.getId());
+        if(peckTable == null){
+            throw new ToClientException("goods is not exist");
+        }
+        if(peckTable.getLimit()> 0){
+            // 购买次数限制
+        }
+        log.info("money buy before , peck id = {},num= {} ",moneyBuyBefore.getId(),moneyBuyBefore.getNum());
 //        goldBuy.getId()
 
         MiGongPB.SCMoneyBuyBefore.Builder builder = MiGongPB.SCMoneyBuyBefore.newBuilder();
         builder.setIsOk(1);
         return new RetPacketImpl(MiGongOpcode.SCMoneyBuyBefore, builder.build().toByteArray());
+    }
+    @Tx
+    @Request(opcode = MiGongOpcode.CSMoneyBuy)
+    public RetPacket moneyBuy(Object clientData, Session session) throws Throwable{
+        MiGongPB.CSMoneyBuy moneyBuy = MiGongPB.CSMoneyBuy.parseFrom((byte[])clientData);
+        PeckTable peckTable = peckTableMap.get(moneyBuy.getId());
+        boolean success = false;
+        int money = 0;
+        try {
+            if (peckTable == null) {
+                throw new ToClientException("goods is not exist");
+            }
+            if (moneyBuy.getNum() <= 0) {
+                throw new ToClientException("invalid param,num");
+            }
+            if (StringUtils.isEmpty(moneyBuy.getToken())) {
+                throw new ToClientException("invalid param,token");
+            }
+
+            // TODO 去支付方验证token，并获取实际支付的钱，赋值给money
+            boolean result = true;
+            if (!result) {
+                throw new ToClientException("order check fail");
+            }
+            money = peckTable.getPrice(); // 这个改为实际支付的钱
+            if(money != peckTable.getPrice()){
+                //
+                log.error("money error! order money = {},peck price = {}",money,peckTable.getPrice());
+            }
+            if(money <= 0){
+                throw new ToClientException("order check fail");
+            }
+
+            log.info("order check success,money = "+money);
+
+            // 发放商品
+            // 金币
+            int gold = addGold(session.getAccountId(), peckTable.getGold() * moneyBuy.getNum());
+            // 道具
+            Map<Integer, Integer> map = Util.split2Map(peckTable.getItems(), Integer.class, Integer.class);
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                itemService.addItem(session.getAccountId(), entry.getKey(), entry.getValue() * moneyBuy.getNum());
+            }
+            success = true;
+        }catch (Throwable e){
+            throw e;
+        }finally {
+            // 添加订单记录
+            Order order = new Order();
+            order.setId(idService.acquireInt(Order.class));
+            order.setGold(peckTable.getGold() * moneyBuy.getNum());
+            order.setSuccess(success?1:0);
+            order.setUserId(session.getAccountId());
+            order.setItems(peckTable.getItems());
+            order.setMoney(money);
+            order.setNum(moneyBuy.getNum());
+            order.setPeckId(peckTable.getId());
+            order.setTime(new Timestamp(System.currentTimeMillis()));
+            order.setToken(moneyBuy.getToken());
+            dataService.insert(order);
+        }
+        //
+
+        MiGongPB.SCMoneyBuy.Builder builder = MiGongPB.SCMoneyBuy.newBuilder();
+        UserMiGong userMiGong = miGongService.get(session.getAccountId());
+        builder.setGold(userMiGong.getGold());
+        builder.setSuccess(success?1:0);
+        return new RetPacketImpl(MiGongOpcode.SCMoneyBuy, builder.build().toByteArray());
     }
 
     @Tx
