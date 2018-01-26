bf0c77794274dc21c2c0465f182942d085d06161
zhengyuzhen <zhengyuzhen@elex-tech.com>
2018-01-26 14:46:55 +0800

    migong

--- code/MMServerEngine/target/classes/start.sh
+++ code/MMServerEngine/target/classes/start.sh
@@ -3,5 +3,5 @@
 #kill
 ./shutdown.sh
 
-nohup java -cp "./lib/*:./:./*:./WEB-INF/*" -Xmx512m -Xms512m -XX:PermSize=128M -Dfile.encoding=UTF-8 com.mm.engine.framework.server.Server | cronolog /usr/migong/logs/dynasty2.out.%Y%m%d%H >/dev/usr/migong/logs/all.txt 2>&1 &
+nohup java -cp "./lib/*:./:./*:./WEB-INF/*" -Xmx512m -Xms512m -XX:PermSize=128M -Dfile.encoding=UTF-8 com.mm.engine.framework.server.Server | cronolog /usr/migong/logs/dynasty2.out.%Y%m%d%H >/usr/migong/logs/all.txt 2>&1 &
 tail -f /usr/migong/logs/dynasty2.out.$(date "+%Y%m%d%H")
